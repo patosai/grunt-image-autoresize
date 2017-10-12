fafc3c1ab5ad26ec170ef8990b8b09b989b2356a
Patrick Tsai <penguin@patosai.com>
2017-10-12 01:41:10 -0400

    Add batching of images

--- tasks/image-autoresize.js
+++ tasks/image-autoresize.js
@@ -5,9 +5,13 @@ var path = require('path');
 
 var Promise = require('promise');
 
+var BATCH_SIZE = 4;
+var DEFAULT_MAX_DIMENSION_PX = 800;
+
 function parseImage(imagePath, maxdimension, outputPath) {
   return new Promise(function(resolve, reject) {
     var image = gm(imagePath);
+
     image.size(function(err, size) {
       if (err) {
         reject(err);
@@ -19,6 +23,7 @@ function parseImage(imagePath, maxdimension, outputPath) {
       } else {
         image.resize(null, maxdimension)
       }
+
       image.write(outputPath, function(err) {
         if (err) {
           reject(err);
@@ -34,25 +39,33 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('image_autoresize', 'Auto-resize images larger than a certain size', function() {
     var done = this.async();
 
-    var maxdimension = this.data.maxdimension || 800;
+    var maxdimension = this.data.maxdimension || DEFAULT_MAX_DIMENSION_PX;
     var fileObjectArray = this.data.files;
 
-    var fileObjectPromises = fileObjectArray.map(function(fileObject) {
-      var fileList = grunt.file.expand(fileObject, fileObject.src);
-      var filePromises = fileList.map(function(file) {
-        var filePath = path.join(fileObject.cwd, file);
-        var outputPath = path.join(fileObject.dest || fileObject.cwd, file);
-        return parseImage(filePath, maxdimension, outputPath);
-      });
-      return Promise.all(filePromises)
-    })
+    var completePromise = Promise.resolve();
 
-    Promise.all(fileObjectPromises)
-      .then(function() {
-        done()
-      })
-      .catch(function(err) {
-        done(err)
-      })
+    fileObjectArray.forEach(function(fileObject) {
+      var fileList = grunt.file.expand(fileObject, fileObject.src);
+
+      for (var ii = 0; ii < fileList.length; ii += BATCH_SIZE) {
+        var fileSlice = fileList.slice(ii, ii + BATCH_SIZE);
+
+        completePromise = completePromise.then(function() {
+          var filePromises = fileSlice.map(function(filename) {
+            var filePath = path.join(fileObject.cwd, filename);
+            var outputPath = path.join(fileObject.dest || fileObject.cwd, filename);
+            return parseImage(filePath, maxdimension, outputPath);
+          });
+
+          return Promise.all(filePromises);
+        });
+      }
+    });
+
+    completePromise.then(function() {
+      done();
+    }).catch(function(err) {
+      done(err);
+    });
   });
 }
